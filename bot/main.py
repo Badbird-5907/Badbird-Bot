db789b51656619c12d478e1fb9f21403647062ff
Badbird-5907 <50347938+Badbird-5907@users.noreply.github.com>
2020-02-29 14:52:40 -0500

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,66 +1,160 @@
-import discord
+import logging
 import os
-from keep_alive import keep_alive
+import discord
 from discord.ext import commands
-bot = commands.Bot(command_prefix='$', description='A bot.')
+from discord.ext.commands import Bot
+import asyncio
+logging.basicConfig(level=logging.INFO)
 client = discord.Client()
-print('----------------------------------------------------------------------------')
-print ("Discord.py API Version:")
-print (discord.__version__)
-print('----------------------------------------------------------------------------')
+nou = discord.Embed()
+nou.set_image(url="https://i.kym-cdn.com/entries/icons/original/000/030/219/uno.png")
+TOKEN = "LMAO U TRIED B00M3R"
+
 @client.event
 async def on_ready():
-    print('----------------------------------------------------------------------------')
-    print("bot is up and running  User ID:")
-    print(client.user)
-    print('----------------------------------------------------------------------------')
+    print ("Discord API version:")
+    print (discord.version_info)
+    print('Logged in as {0.user}'.format(client))
+    print ("Token:")
+    print (TOKEN)
+
+print ("-------------------------------------------------------------")
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == '!help':
+        await message.channel.send('Here are some commands:')
+        await message.channel.send("!uno-reverse red,blue,green,yellow")
+        await message.channel.send('ping')
+        await message.channel.send('SHIT')
 
 
-@bot.command()
-async def add(ctx, a: int, b: int):
-    await ctx.send(a + b)
+    if message.content == '!uno-reverse red':
+            await message.channel.send(file=discord.File('images/images/unored.png'))
 
-@bot.command()
-async def foo(ctx, arg):
-    await ctx.send(arg)
+    if message.content == '!uno-reverse blue':
+                    await message.channel.send(file=discord.File('images/images/unoblue.png'))
 
+    if message.content == '!uno-reverse green':
+                    await message.channel.send(file=discord.File('images/images/unogreen.png'))
 
-@bot.command(name='list')
-async def _list(ctx, arg):
-    pass
+    if message.content == '!uno-reverse yellow':
+                    await message.channel.send(file=discord.File('images/images/unoyellow.png'))
 
-@bot.command()
-async def test(ctx, arg):
-    await ctx.send(arg)
-    
-@bot.command()
-async def info(ctx):
-    embed = discord.Embed(title="Bot", description="A bot.", color=0xeee657)
+    if message.content == 'ping':
+        await message.channel.send('pong')
 
+    if message.content == 'SHIT':           
+        await message.channel.send(file=discord.File('images/gif/rhinopoo.gif'))
+
+    if message.content == 'Shit':
+            await message.channel.send(file=discord.File('images/gif/rhinopoo.gif'))
+    if message.content == 'shit':
+                    await message.channel.send(file=discord.File('images/gif/rhinopoo.gif'))
    
-    embed.add_field(name="Author", value="Badbird 5907")
+    if message.content == 'DIE Badbird Bot':
+                await message.channel.send('NOU')
+                await message.channel.send('UNO REVERSE')
+                await message.channel.send(file=discord.File('images/images/nou.png'))
 
-    # Shows the number of servers the bot is member of.
-    embed.add_field(name="Server count", value=f"{len(bot.guilds)}")
+    if message.content == 'die badbird bot':
+                        await message.channel.send('NOU')
+                        await message.channel.send('UNO REVERSE')
+                        await message.channel.send(file=discord.File('images/images/nou.png'))
 
+    if message.content == 'Die badbird bot':
+                        await message.channel.send('NOU')
+                        await message.channel.send('UNO REVERSE')
+                        await message.channel.send(file=discord.File('images/images/nou.png'))
+
+
+    if message.content == 'die Badbird Bot':
+                        await message.channel.send('NOU')
+                        await message.channel.send('UNO REVERSE')
+                        await channel.send(file=discord.File('images/images/nou.png'))
+
+
+    if message.content == 'DIE BADBIRD BOT':
+                        await message.channel.send('NOU')
+                        await message.channel.send('UNO REVERSE')
+                        await message.channel.send(file=discord.File('images/images/nou.png'))
+
+    if message.content.startswith('Stop'):
+        await message.channel.send('NO!')
+
+    if message.content.startswith('stop'):
+        await message.channel.send('NO!')
+
+    if message.content.startswith('STOP'):
+        await message.channel.send('NO!')
+      
+    if message.content.startswith('STAHP'):
+        await message.channel.send('NO!')
+
+    if message.content.startswith('stahp'):
+        await message.channel.send('NO!')
+     if message.content.startswith('Stahp'):
+        await message.channel.send('NO!')
+
+    if message.content.startswith('k'):
+        await message.channel.send('k')
     
-    embed.add_field(name="Invite", value="[Invite link](<insert your OAuth invitation link here>)")
+    if message.content.startswith('K'):
+        await message.channel.send('k')
 
-    await ctx.send(embed=embed)
+    if message.content.startswith('hi'):
+        await message.channel.send('Hello!')
+
+    if message.content.startswith('Hi'):
+        await message.channel.send('Hello!')
+
+    if message.content.startswith('HI'):
+        await message.channel.send('Hello!')
+
+    if message.content.startswith('f'):
+        await message.channel.send('F')
+
+    if message.content.startswith('F'):
+        await message.channel.send('F')
+
+    if message.content.startswith('lol'):
+        await message.channel.send('LMAO')
+
+    if message.content.startswith('LOL'):
+        await message.channel.send('LMAO')
+
+    if message.content.startswith('Lol'):
+        await message.channel.send('LMAO')
+
+    if message.content.startswith('lmao'):
+        await message.channel.send('LMAO!')
+
+    if message.content.startswith('LMAO'):
+        await message.channel.send('LMAO!')
+
+    if message.content.startswith('Lmao'):
+        await message.channel.send('LMAO!')
+
+    if message.content.startswith('lmao'):
+        await message.channel.send('LMAO!')
+    if message.content.startswith('ok'):
+        await message.channel.send('B00m3r')
+
+    if message.content.startswith('Ok'):
+        await message.channel.send('B00m3r')
+
+    if message.content.startswith('OK'):
+        await message.channel.send('B00M3R')
     
-@bot.command()
-async def helpme(ctx):
-    embed = discord.Embed(title="nice bot", description="A Very Nice bot. List of commands are:", color=0xeee657)
+    if message.content.startswith('fuck'):
+        await message.channel.send('Calm Down B00M3R')
 
-    embed.add_field(name="$add X Y", value="Gives the addition of **X** and **Y**", inline=False)
-    embed.add_field(name="$multiply X Y", value="Gives the multiplication of **X** and **Y**", inline=False)
-    embed.add_field(name="$greet", value="Gives a nice greet message", inline=False)
-    embed.add_field(name="$info", value="Gives a little info about the bot", inline=False)
-    embed.add_field(name="$help",  value="Gives this message", inline=False)
+    if message.content.startswith('Fuck'):
+        await message.channel.send('Calm Down B00M3R')
 
-    await ctx.send(embed=embed)
-
-
-keep_alive()
-token = os.environ.get("TOKEN")
-client.run(token)
+    if message.content.startswith('FUCK'):
+        await message.channel.send('Calm Down B00M3R')
+        
+client.run(TOKEN)
