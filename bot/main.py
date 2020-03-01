4ebf28196b379410cca5e80dabec0778eb6d05fb
Badbird-5907 <50347938+Badbird-5907@users.noreply.github.com>
2020-02-29 20:35:15 -0500

    Update main.py
    added !help embed new command, watching !help status,

--- bot/main.py
+++ bot/main.py
@@ -8,27 +8,38 @@ logging.basicConfig(level=logging.INFO)
 client = discord.Client()
 nou = discord.Embed()
 nou.set_image(url="https://i.kym-cdn.com/entries/icons/original/000/030/219/uno.png")
-TOKEN = "LMAO U TRIED B00M3R"
+TOKEN = "NjMwNDQwODYwNTY3NDcwMTMw.Xlc7qQ.lQtwSAceni1d0__B2VXE17NNKgo"
 
 @client.event
 async def on_ready():
+    print ("--------------------------------------------------------------")
     print ("Discord API version:")
     print (discord.version_info)
     print('Logged in as {0.user}'.format(client))
     print ("Token:")
     print (TOKEN)
+    print ("--------------------------------------------------------------")
+    await client.change_presence(activity=discord.Game(name='!help'))
 
-print ("-------------------------------------------------------------")
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-
     if message.content == '!help':
-        await message.channel.send('Here are some commands:')
-        await message.channel.send("!uno-reverse red,blue,green,yellow")
-        await message.channel.send('ping')
-        await message.channel.send('SHIT')
+        embed = discord.Embed(title="Commands", description="Here are some usefull commands:", color=0x00ff00)
+        embed.add_field(name="!uno-reverse", value="<red/blue/yellow/green>", inline=False)
+        embed.add_field(name="!ping", value="pong", inline=False)
+        embed.add_field(name="!roast-me", value="<will roast you>", inline=False)
+        embed.add_field(name="say some swear words", value="<sh!t/fuc*>", inline=False)
+        embed.add_field(name="say ok", value="<B00m3r>", inline=False)
+        embed.add_field(name="say k", value="<k>", inline=False)
+        embed.add_field(name="say f", value="<F>", inline=False)
+        embed.add_field(name="say Die Badbird Bot", value="<a suprise>", inline=False)
+        embed.add_field(name="And much more...", value="there is a lot more to be discoverd...",  inline=False)
+        await message.channel.send(embed=embed)
+
+    if message.content.startswith('!roast-me'):
+            await message.channel.send('I was told not to roast trash because it smells like SH!T')
 
 
     if message.content == '!uno-reverse red':
@@ -36,18 +47,17 @@ async def on_message(message):
 
     if message.content == '!uno-reverse blue':
                     await message.channel.send(file=discord.File('images/images/unoblue.png'))
-
     if message.content == '!uno-reverse green':
                     await message.channel.send(file=discord.File('images/images/unogreen.png'))
 
     if message.content == '!uno-reverse yellow':
                     await message.channel.send(file=discord.File('images/images/unoyellow.png'))
 
-    if message.content == 'ping':
+    if message.content == '!ping':
         await message.channel.send('pong')
 
-    if message.content == 'SHIT':           
-        await message.channel.send(file=discord.File('images/gif/rhinopoo.gif'))
+    if message.content == 'SHIT':
+            await message.channel.send(file=discord.File('images/gif/rhinopoo.gif'))
 
     if message.content == 'Shit':
             await message.channel.send(file=discord.File('images/gif/rhinopoo.gif'))
@@ -73,7 +83,7 @@ async def on_message(message):
     if message.content == 'die Badbird Bot':
                         await message.channel.send('NOU')
                         await message.channel.send('UNO REVERSE')
-                        await channel.send(file=discord.File('images/images/nou.png'))
+                        await message.hannel.send(file=discord.File('images/images/nou.png'))
 
 
     if message.content == 'DIE BADBIRD BOT':
@@ -86,7 +96,6 @@ async def on_message(message):
 
     if message.content.startswith('stop'):
         await message.channel.send('NO!')
-
     if message.content.startswith('STOP'):
         await message.channel.send('NO!')
       
@@ -95,7 +104,17 @@ async def on_message(message):
 
     if message.content.startswith('stahp'):
         await message.channel.send('NO!')
-     if message.content.startswith('Stahp'):
+
+    if message.content.startswith('Stahp'):
+        await message.channel.send('NO!')
+
+    if message.content.startswith('Sthap'):
+        await message.channel.send('NO!')
+
+    if message.content.startswith('STHAP'):
+        await message.channel.send('NO!')
+
+    if message.content.startswith('sthap'):
         await message.channel.send('NO!')
 
     if message.content.startswith('k'):
@@ -124,7 +143,6 @@ async def on_message(message):
 
     if message.content.startswith('LOL'):
         await message.channel.send('LMAO')
-
     if message.content.startswith('Lol'):
         await message.channel.send('LMAO')
 
@@ -139,6 +157,7 @@ async def on_message(message):
 
     if message.content.startswith('lmao'):
         await message.channel.send('LMAO!')
+
     if message.content.startswith('ok'):
         await message.channel.send('B00m3r')
 
@@ -156,5 +175,4 @@ async def on_message(message):
 
     if message.content.startswith('FUCK'):
         await message.channel.send('Calm Down B00M3R')
-        
 client.run(TOKEN)
