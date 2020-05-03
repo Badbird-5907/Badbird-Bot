0886902c2b369d01bd80e7b19ad22ed391b2b96a
Badbird-5907 <50347938+Badbird-5907@users.noreply.github.com>
2020-05-02 22:37:15 -0400

    Update bot-new.py

--- bot/bot-new.py
+++ bot/bot-new.py
@@ -10,6 +10,12 @@ nou = discord.Embed()
 bot = commands.Bot(command_prefix='b!')
 TOKEN = "fff"
 
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
 @client.event
 async def on_ready():
     print ("--------------------------------------------------------------")
@@ -20,8 +26,12 @@ async def on_ready():
     print (TOKEN)
     print ("--------------------------------------------------------------")
     await client.change_presence(activity=discord.Game(name='b!help'))
-@bot.command()
-async def foo(ctx, arg):
-    await ctx.send(arg)
-    
+@bot.command(name+'help')
+async def help():
+    await send(
+    embed=discord.Embed(title="Commands", description="Some usefull commands", color=0x00ff00)
+    embed.set_author(name="a")
+    embed.add_field(name="a", value="undefined", inline=False)
+    await self.bot.say(embed=embed)
+    )
 bot.run(TOKEN)
