db78d1d5aa09e1cb92290bd0faa86a80587a13ad
Badbird-5907 <50347938+Badbird-5907@users.noreply.github.com>
2020-05-03 14:20:47 -0400

    Update bot-new.py

--- bot/bot-new.py
+++ bot/bot-new.py
@@ -21,10 +21,15 @@ async def info(ctx):
     embed.add_field(name="Author", value="Badbird 5907")
     # Shows the number of servers the bot is member of.
     embed.add_field(name="Server count", value=f"{len(bot.guilds)}")
-    # give users a link to invite thsi bot to their server
+    # give users a link to invite this bot to their server
     embed.add_field(name="Invite", value="[Invite link]https://bot.badbird5907.net")
     await ctx.send(embed=embed)
-
+@bot.command()
+async def help(ctx):
+    embed = discord.Embed(title="Commands", description="Some Usefull commands", color=ff1100)
+    embed.add_feild(name="b!help", value="shows this")
+    embed.add_feild(name="test"),  value="test")
+    await ctx.send(embed=embed)
 @bot.command()
 async def add(ctx, a: int, b: int):
     await ctx.send(a+b)
@@ -32,4 +37,12 @@ async def add(ctx, a: int, b: int):
 @bot.command()
 async def multiply(ctx, a: int, b: int):
     await ctx.send(a*b)
+@bot.command()
+async def say(ctx):
+    await ctx.send(ctx)
+@bot.command
+async def unoreverse(ctx):
+    if (ctx = red)
+    elif (ctx = blue)
+    elif (ctx = green)
 bot.run(TOKEN)                          
