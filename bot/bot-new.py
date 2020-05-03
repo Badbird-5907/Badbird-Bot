827104b2b5e813b0bd19d0c7255192d875894d4d
Badbird-5907 <50347938+Badbird-5907@users.noreply.github.com>
2020-05-03 13:59:42 -0400

    rewrite

--- bot/bot-new.py
+++ bot/bot-new.py
@@ -1,6 +1,5 @@
 #Badbird Bot BETA
 #--SETUP START--#
-import os
 import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
@@ -11,16 +10,14 @@ nou = discord.Embed()
 bot = commands.Bot(command_prefix='b!')
 TOKEN = ""
 #--SETUP END--#
-@client.event
-async def on_ready():
-    print ("--------------------------------------------------------------")
-    print ("Discord API version:", discord.version_info)
-    print('Logged in as {0.user}'.format(client))
-    print ("Token:", TOKEN)
-    print ("--------------------------------------------------------------")
-
+@client.event()
+async def on_ready()
+    print("---Bot Info---")
+    print("Discord API Version:", discord.version_info)
+    print("Logged in as {0.user}".format(client))
+    print("---Bot Info---")
 @bot.command()
-async def info(ctx):
+async def info(ctx)
     embed = discord.Embed(title="Badbird Bot", description="Bot created by Badbird5897.", color=0xeee657)
     # give info about you here
     embed.add_field(name="Author", value="Badbird 5907")
@@ -30,17 +27,6 @@ async def info(ctx):
     embed.add_field(name="Invite", value="[Invite link]https://bot.badbird5907.net")
     await ctx.send(embed=embed)
 
-@bot.command()
-async def help(ctx):
-    embed = discord.Embed(title="Badbird Bot", description="Bot created by Badbird5897.", color=0xeee657)
-    # give info about you here
-    embed.add_field(name="Author", value="Badbird 5907")
-    # Shows the number of servers the bot is member of.
-    embed.add_field(name="Server count", value=f"{len(bot.guilds)}")
-    # give users a link to invite thsi bot to their server
-    embed.add_field(name="Invite", value="[Invite link]https://bot.badbird5907.net")
-    await ctx.send(embed=embed)
-    
 @bot.command()
 async def add(ctx, a: int, b: int):
     await ctx.send(a+b)
